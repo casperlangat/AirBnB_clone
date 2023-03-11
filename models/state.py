d0d76d1dd590afed806d1122c3d4e628faa9c40b
casperlangat <langatcasper@gmail.com>
2023-03-11 05:44:20 -0800

    AirBnB clone - The console Project

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/sr/bin/python3
+"""This module creates a user class"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+	"""Class for managing state objects"""
+
+	name = ""
---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module creates a user class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+	"""Class for managing user objectts"""
+
+	email = ""
+	password = ""
+	first_name = ""
+	last_name = ""
